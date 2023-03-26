ebf849d5a749f3d1e8508b5d54dd54e57b4f0343
1027354 <tim25659@gmail.com>
2023-03-26 17:01:58 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -9,16 +10,33 @@ def main():
     # Uncomment this block to pass the first stage
 
     command = sys.argv[1]
+    args = sys.argv[2:]
     if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/master\n")
-        print("Initialized git directory")
+        init()
+    elif command == "cat-file":
+        cat_file(*args)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
+
+def init():
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/master\n")
+    print("Initialized git directory1")
+
+
+def cat_file(*args):
+    file = None
+    if len(args) < 2:
+        file = args[0]
+    if args[0] == "-p":
+        file = args[1]
+    folder, file = file[:2], file[2:]
+    with open(f".git\objects\{folder}/{file}", "rb") as f:
+        sys.stdout.write(zlib.decompress(f.read()).decode("utf-8")[8:])
 
 if __name__ == "__main__":
     main()
